b358a578cc917f3c4dbd83467a028414bef9239a
JHLBLUE <ljho2321@gmail.com>
2020-11-29 23:19:30 +0900

    Init
    Init Android files

---
+++ app/src/main/java/com/example/androidstudy/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button0, buttonDelete, buttonEnter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button_1);
+    }
+}
---
+++ app/src/main/java/com/example/androidstudy/SecondsActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidstudy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
